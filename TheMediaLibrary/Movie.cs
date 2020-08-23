2158185ecbab31a59b0f620b9d46b5954a65bd01
thatgeekbrandon <68824480+thatgeekbrandon@users.noreply.github.com>
2020-08-22 22:29:35 -0600

    TheMediaLibrary initial commit

---
+++ TheMediaLibrary/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+    class Movie
+    {
+        public string Title;
+        public string Director;
+    }
+}
---
+++ TheMediaLibrary/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Treehouse.MediaLibrary
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+    }
+    class Album
+    {
+        public string Title;
+        public string Artist;
+    }
+    class Movie
+    {
+        public string Title;
+        public string Director;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
